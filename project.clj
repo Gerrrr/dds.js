985df47b5764acf6c5297079526af5d16d6345b8
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2016-01-30 23:40:34 +0100

    css minimization introduced

--- project.clj
+++ project.clj
@@ -3,21 +3,23 @@
   :url "http://example.com/FIXME"
   :license {:name "Apache License Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :plugins [[lein-ancient "0.6.8"]
-            [lein-cljsbuild "1.1.1"]
-            [lein-figwheel "0.5.0-1"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
-                 [prismatic/schema "1.0.3"]
+                 [prismatic/schema "1.0.4"]
                  [prismatic/plumbing "0.5.2"]
                  [devcards "0.2.1"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]]
   :figwheel {:css-dirs ["resources/public/css"]}
+  :profiles
+  {:dev {:plugins [[lein-ancient "0.6.8"]
+                   [lein-cljsbuild "1.1.1"]
+                   [lein-figwheel "0.5.0-1"]
+                   [lein-asset-minifier "0.2.4"]]}}
   :cljsbuild
   {:builds
    [{:id "dev"
      :source-paths ["src/cljs/" "src-dev/cljs"]
-     :figwheel {:devcards true }
+     :figwheel {:devcards true}
      :compiler {:output-to "resources/public/js/dev/compiled/dds.js"
                 :output-dir "resources/public/js/dev/compiled/out"
                 :asset-path "js/dev/compiled/out"
@@ -32,4 +34,6 @@
                 :whitespace false
                 :optimizations :advanced
                 :externs ["resources/externs/c3.js"]}
-     :notify-command ["scripts/build.sh"]}]})
+     :notify-command ["scripts/build.sh"]}]}
+  :minify-assets
+  {:assets {"resources/public/css/dds.min.css" "resources/public/css/dds"}})
