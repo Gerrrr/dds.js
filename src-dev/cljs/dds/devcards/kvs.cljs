006adfcc6d08b7e678767d4d9160c4ac6012df71
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2016-02-06 19:11:17 +0100

    fix kvs devcards

--- src-dev/cljs/dds/devcards/kvs.cljs
+++ src-dev/cljs/dds/devcards/kvs.cljs
@@ -10,5 +10,5 @@
   (dc/dom-node
    (fn [data-atom node]
      (let [seq [["hello" "world"] ["foo" "bar"] ["a" "b"]]
-           chart (dds/key-value-sequence "title" seq)]
+           chart (dds/key-value-seq "title" seq)]
        (ddu/set-content! node chart)))))
