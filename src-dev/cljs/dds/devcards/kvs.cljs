d77057b99ecf132e9b74bccdfa1f127166560208
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2016-02-10 23:06:47 +0100

    fix table and kvs devcards

--- src-dev/cljs/dds/devcards/kvs.cljs
+++ src-dev/cljs/dds/devcards/kvs.cljs
@@ -9,6 +9,8 @@
 (defcard "## Key Value Sequence"
   (dc/dom-node
    (fn [data-atom node]
-     (let [seq [["hello" "world"] ["foo" "bar"] ["a" "b"]]
+     (let [seq [{:key "hello" :val "world"}
+                {:key "foo" :val "bar"}
+                {:key "a" :val "b"}]
            chart (dds/key-value-seq "title" seq)]
        (ddu/set-content! node chart)))))
