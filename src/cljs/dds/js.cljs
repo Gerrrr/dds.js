48ee64ecce65df77a0eda7e46e2de65863e4e5c1
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2016-02-12 13:48:05 +0100

    function names in js api changed to camel case

--- src/cljs/dds/js.cljs
+++ src/cljs/dds/js.cljs
@@ -5,13 +5,13 @@
 (defn- transform-args [args]
   (map #(js->clj % :keywordize-keys true) args))
 
-(defn ^:export barchart [& args]
+(defn ^:export barChart [& args]
   (apply dds/barchart (transform-args args)))
 
-(defn ^:export piechart [& args]
+(defn ^:export pieChart [& args]
   (apply dds/piechart (transform-args args)))
 
-(defn ^:export scatterplot [& args]
+(defn ^:export scatterPlot [& args]
   (apply dds/scatterplot (transform-args args)))
 
 (defn ^:export histogram [& args]
@@ -20,7 +20,7 @@
 (defn ^:export heatmap [& args]
   (apply dds/heatmap (transform-args args)))
 
-(defn ^:export key_value_sequence [& args]
+(defn ^:export keyValueSequence [& args]
   (apply dds/key-value-seq (transform-args args)))
 
 (defn ^:export graph [& args]
