d109eec96c74240d0c7b701c9166bf1a0b4710d3
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2016-02-12 13:37:08 +0100

    remove deep-merge from utils

--- src/cljs/dds/utils.cljs
+++ src/cljs/dds/utils.cljs
@@ -1,24 +1,6 @@
 (ns dds.utils
   (:require [cljs-uuid-utils.core :as uuid]))
 
-(defn deep-merge-with
-  "Like merge-with, but merges maps recursively, appling the given fn
-  only when there's a non-map at a particular level.
-
-  (deepmerge + {:a {:b {:c 1 :d {:x 1 :y 2}} :e 3} :f 4}
-  {:a {:b {:c 2 :d {:z 9} :z 3} :e 100}})
-  -> {:a {:b {:z 3, :c 3, :d {:z 9, :x 1, :y 2}}, :e 103}, :f 4}
-
-  taken from http://dev.clojure.org/jira/browse/CLJ-1468
-  "
-  [f & maps]
-  (apply
-   (fn m [& maps]
-     (if (every? map? maps)
-       (apply merge-with m maps)
-       (apply f maps)))
-   maps))
-
 (defn get-parent-rect-value [node key]
   "Copied from https://github.com/masayuki0812/c3/blob/f56e853237ca84c372fd5dcb795491a8ebc6468d/c3.js#L2699"
   (loop [node node]
